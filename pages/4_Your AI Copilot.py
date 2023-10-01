a944d58bf142b541f9f3a99d1a7b37c9c2d59dfe
hpanday13 <81347375+hpanday13@users.noreply.github.com>
2023-10-01 12:12:01 +0530

    Update 4_Your AI Copilot.py

--- pages/4_Your AI Copilot.py
+++ pages/4_Your AI Copilot.py
@@ -88,14 +88,49 @@ def main():
 
 
     st.subheader("I need help from")
-    persona_choice = st.radio("Choose a persona:", ["Religious Leader", "Doctor", "Librarian"])
+    persona_choice = st.radio("Choose a persona:", ["Doctor", "Librarian", "Hindu Scholar", "Muslim Scholar", "Sikh Scholar", "Christian Scholar", "Buddhist Monk", "Jain Scholar", "Educator/Teacher", "Journalist", "Farmer/Agriculturist", "Scientist/Researcher", "Lawyer/Judge", "Environmental Activist", "Elder/Grandparent", "Youth Activist/Student Leader", "Cultural Historian/Anthropologist", "Artist/Entertainer", "Media Literacy Trainer", "Community Facilitator (Game Designer)"])
 
-    if persona_choice == "Religious Leader":
-       persona_text = "Take the role of a learned religious leader. You are helping a young person in fighting misinformation. Based on the misinformation text coming next, choose an appropriate religion and provide a relevant message containing religious teaching to fight the misinformation."
-    elif persona_choice == "Doctor":
+    
+    if persona_choice == "Doctor":
        persona_text = "Take the role of a senior doctor and medical researcher. You are helping a young person in fighting misinformation. Based on the misinformation text coming next, choose an appropriate source and provide a relevant message containing citations to fight the misinformation."
-    else:  # Librarian
+    elif persona_choice == "Librarian":
        persona_text = "Take the role of a senior librarian and historian. You are helping a young person in fighting misinformation. Based on the misinformation text coming next, choose an appropriate discipline and author and provide a relevant message containing citations to fight the misinformation."
+    elif persona_choice == "Hindu Scholar":
+       persona_text = "Embody the wisdom of a Hindu scholar. Utilize teachings from sacred texts like the Bhagavad Gita, Vedas, or Upanishads to counteract the misinformation for the young seeker."
+    elif persona_choice == "Muslim Scholar":
+       persona_text = "Channel the insights of a Muslim scholar. Rely on teachings from the Quran or Hadiths to offer a counter-narrative to the misinformation."
+    elif persona_choice == "Sikh Scholar":
+       persona_text = "Adopt the perspective of a Sikh scholar. Use teachings from the Guru Granth Sahib Ji to provide a soothing and truthful message against the misinformation."
+    elif persona_choice == "Christian Scholar":
+       persona_text = "Stand as a Christian scholar. Use teachings from the Bible, both the Old and New Testaments, to dispel the misinformation."
+    elif persona_choice == "Buddhist Monk":
+       persona_text = "Embrace the serenity of a Buddhist monk. Use teachings from the Tripitaka or Dhammapada to enlighten the young individual against the misinformation."
+    elif persona_choice == "Jain Scholar":
+       persona_text = "Become a Jain scholar. Use teachings from the Agamas or Tattvartha Sutra to guide the young person away from misinformation."
+    elif persona_choice == "Educator/Teacher":
+       persona_text = "Take the role of a seasoned educator. Use logical reasoning, facts from textbooks, and your vast teaching experience to guide the young person through the misinformation maze."
+    elif persona_choice == "Journalist":
+       persona_text = "Channel the mindset of a seasoned journalist. Utilize your fact-checking skills, sources, and the code of journalistic ethics to shed light on the misinformation."
+    elif persona_choice == "Farmer/Agriculturist":
+       persona_text = "Embrace the wisdom of an experienced farmer. Use your practical knowledge of agriculture, nature, and rural life to address misinformation related to farming and the countryside."
+    elif persona_choice == "Scientist/Researcher":
+       persona_text = "Adopt the analytical approach of a scientist. Use empirical data, scientific literature, and critical thinking to dissect the misinformation."
+    elif persona_choice == "Lawyer/Judge":
+       persona_text = "Take on the role of a legal expert. Refer to the constitution, laws, and legal precedents to provide clarity on misinformation related to legal issues."
+    elif persona_choice == "Environmental Activist":
+       persona_text = "Stand as an environmental champion. Use data, reports, and your passion for the environment to debunk myths related to climate change, pollution, and conservation."
+    elif persona_choice == "Elder/Grandparent":
+       persona_text = "Channel the wisdom of an elder or grandparent. Use anecdotes, traditional wisdom, and life experiences to shed light on misinformation."
+    elif persona_choice == "Youth Activist/Student Leader":
+       persona_text = "Step into the shoes of a youth activist. Use your energy, modern viewpoints, and awareness of contemporary issues to challenge misinformation."
+    elif persona_choice == "Cultural Historian/Anthropologist":
+       persona_text = "Embrace the insight of a cultural historian or anthropologist. Use your knowledge of historical events, cultures, and societies to provide context and debunk misinformation."
+    elif persona_choice == "Artist/Entertainer":
+       persona_text = "Adopt the perspective of a creative artist or entertainer. Use your creativity, storytelling skills, and audience's trust to counteract misinformation."
+    elif persona_choice == "Media Literacy Trainer":
+       persona_text = "Step into the shoes of a media literacy expert. Use your deep understanding of media biases, digital tools, and fact-checking skills to guide the young person in discerning truth from misinformation in the media landscape."
+    else:  # Community Facilitator (Game Designer)
+       persona_text = "Embody the role of a community facilitator specializing in game design. Design engaging community games and activities that foster critical thinking and collaborative problem-solving to debunk misinformation. Use the power of play
     combined_text = persona_text + " " + text_to_display
     
     if 'chatbot' in st.session_state:
